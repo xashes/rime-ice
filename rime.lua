54ded1f147e6dbc1d8417615e03894d6df5a4a11
xashes <xashes@outlook.com>
2023-07-04 22:22:33 +0800

    use jidian dict, and copy lua code from jidian config

--- rime.lua
+++ rime.lua
@@ -557,3 +557,47 @@ function time_date(input, seg,env)
 	number_translator(input, seg)
 	set_switch_keywords(input, seg,env)
 end
+
+-- from jidian wubi
+--- 过滤器：单字在先
+function single_char_first_filter(input)
+    local l = {}
+    for cand in input:iter() do
+        if (utf8.len(cand.text) == 1) then
+            yield(cand)
+        else
+            table.insert(l, cand)
+        end
+    end
+    for i, cand in ipairs(l) do
+        yield(cand)
+    end
+end
+
+--- 过滤器：只显示单字
+function single_char_only(input)
+    for cand in input:iter() do
+        if (utf8.len(cand.text) == 1) then
+            yield(cand)
+        end
+    end
+end
+
+--- 计算器
+--- @KyleBing 2022-01-17
+function calculator(input, seg)
+    if string.find(input, 'coco') ~= nil then -- 匹配 coco 开头的字符串
+        local _, _, a, operation, b = string.find(input, "coco(%d+%.?%d*)([%+%-%*/])(%d+%.?%d*)")
+        local result = 0
+        if operation == '+' then
+            result = a + b
+        elseif operation == '-' then
+            result = a - b
+        elseif operation == '*' then
+            result = a * b
+        elseif operation == '/' then
+            result = a / b
+        end
+        yield(Candidate("coco", seg.start, seg._end, result, "计算器"))
+    end
+end
